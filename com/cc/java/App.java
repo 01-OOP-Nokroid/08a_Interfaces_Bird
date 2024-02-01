6f40231c2b1c59659aa15f22c408417917524e65
Nekroid <philipleinung@aol.de>
2024-02-01 10:24:34 +0100

    IFeathers ++

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -2,13 +2,16 @@ package com.cc.java;
 
 public class App {
     
-    public static void main(String[] args) {
-
-      
+    public static void main(String[] args)
+    {
+        Bird bird = new Bird();
+        output(bird.fly());
+        output(bird.hasFeathers());
      
     }
 
-    private static void output(String outputStr) {
+    private static void output(String outputStr)
+    {
         System.out.println(outputStr);
     }
 
